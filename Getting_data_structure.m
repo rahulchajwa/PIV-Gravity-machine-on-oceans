384685374f043fab5f50c53a1eb1d3d5427adfc9
Rahul Chajwa <71154739+rahulchajwa@users.noreply.github.com>
2024-08-12 12:59:18 -0700

    Add files via upload

---
+++ Getting_data_structure.m
@@ -0,0 +1,20 @@
+%% Run this script in the data source folder
+
+clearvars; currentDir = pwd;
+D = dir;
+% getting the labels of all folders and subfolders
+for ii=3:length(D) %note that the first two elements in the directory are garbage
+Folder{ii} = sprintf(D(ii).name);
+cd(D(ii).name);
+DD = dir;
+DL(ii-2) = length(DD)-2;
+for jj=3:length(DD)
+ParentFolder{ii,jj} = sprintf(DD(jj).folder);
+SubFolder{ii,jj} = strcat(DD(jj).folder,'/',DD(jj).name);
+%cd(SubFolder{ii,jj});
+%cd('registered_output');
+%a = dir('*.tif');
+end
+cd ..
+clearvars DD
+end
